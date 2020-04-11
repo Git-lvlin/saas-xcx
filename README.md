2b60322718d005c2e8a7f3618c759b2df9e40487
leeyi <leeyisoft@icloud.com>
2020-04-11 11:10:04 +0800

    init "version": "1.1.37"

---
+++ README.md
@@ -0,0 +1,6 @@
+
+```
+git clone http://git.daihuoduo.com/shop/wechat.miniprogram.git
+cd wechat.miniprogram
+cp siteinfo.demo.js  siteinfo.js
+```
---
+++ siteinfo.demo.js
@@ -0,0 +1,5 @@
+module.exports = {
+  name: "抖商带货",
+  siteroot: "https://test.shop.daihuoduo.com/", // 必填: api地址
+  uniacid: "10001", // 此处为商城ID, 可在超管后台-商城列表中查看
+};
