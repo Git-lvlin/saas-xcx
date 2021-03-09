ed5f48dbdb24f53148c9e61783040b55cd53b93a
daa1 <zonggeu@gmail.com>
2021-03-09 18:34:05 +0800

    commit 油卡翻页加载

--- pages/oilcard/index.js
+++ pages/oilcard/index.js
@@ -10,35 +10,88 @@ Page({
     page: [],
 
     // show
-    notcont: false
+    notcont: false,
+
+    isLoading: true, // 是否正在加载中
+    current_page: 1, // 当前页码
+    last_page: 1, // 最后页码
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function(options) {
+  onLoad: function (options) {
     // 当前页面参数
     this.data.options = options;
+
+    // 设置scroll-view高度
+    this.setListHeight();
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function() {
+  onShow: function () {
     // 获取优惠券列表
     this.getPagelist();
+  },
+
+
+  /**
+   * 设置商品列表高度
+   */
+  setListHeight() {
+    let systemInfo = wx.getSystemInfoSync(),
+      rpx = systemInfo.windowWidth / 750, // 计算rpx
+      tapHeight = Math.floor(rpx * 88), // tap高度
+      scrollHeight = systemInfo.windowHeight - tapHeight; // swiper高度
+    this.setData({
+      scrollHeight
+    });
+  },
+
+
+  /**
+   * 下拉到底加载数据
+   */
+  bindDownLoad() {
+
+    // 已经是最后一页
+    if (this.data.current_page >= this.data.last_page) {
+      this.setData({
+        notcont: true
+      });
+      return false;
+    }
+    // 加载下一页列表
+    this.getPagelist(true, ++this.data.current_page);
   },
 
   /**
    * 获取优惠券列表
    */
-  getPagelist: function() {
+  getPagelist(isPage, page) {
     let _this = this;
-    App._get('oilcard.Oilcard/page', {listRows: 2}, function(result) {
-      _this.setData({
-        page: result.data.data,
-        notcont: !result.data.total
-      });
+    App._get('oilcard.Oilcard/page', {
+      listRows: 8,
+      page: page || 1
+    }, function (result) {
+      let resList = result.data.data,
+        dataList = _this.data.page;
+      if (isPage) {
+        _this.setData({
+          page: dataList.concat(resList),
+          current_page: page,
+          last_page: result.data.last_page,
+          notcont: !result.data.total
+        });
+      } else {
+        _this.setData({
+          page: resList,
+          last_page: result.data.last_page,
+          notcont: !result.data.total
+        });
+      }
     });
   },
 
@@ -46,13 +99,13 @@ Page({
   /**
    * 绑定油卡
    */
-  bindOilcardPage: function() {
+  bindOilcardPage: function () {
     wx.navigateTo({
       url: './bind?'
     })
   },
 
-  editOilcard: function(e) {
+  editOilcard: function (e) {
     var oilcard_id = e.currentTarget.dataset.oilcard_id;
     var oilcard_type = e.currentTarget.dataset.oilcard_type;
     var oilcard_number = e.currentTarget.dataset.oilcard_number;
@@ -65,7 +118,7 @@ Page({
     })
   },
 
-  oilcardRecharge: function(e) {
+  oilcardRecharge: function (e) {
     console.log('e ', e)
     var oilcard_id = e.currentTarget.dataset.oilcard_id;
     var oilcard_type = e.currentTarget.dataset.oilcard_type;
