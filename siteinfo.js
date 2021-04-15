889d72b4ea04415fe2646824390e1d9ce0f6935b
leeyi <leeyisoft@icloud.com>
2021-04-15 21:38:00 +0800

    忽略 siteinfo.js 删除多余文件

--- siteinfo.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  name: "泉天下",
-  siteroot: "https://test.shop.kjcms.cn/", // 必填: api地址
-  AppID: 'wx216126b8e918af11',
-  AppSecret: '56da5a7263bce7833cb725b8a9e75ebb',
-  uniacid: "10001", // 此处为商城ID, 可在超管后台-商城列表中查看
-};
